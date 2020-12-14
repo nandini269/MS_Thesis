882b63cfe0c972ff4c39bfacc43f535a42d7483a
Nandini Nayar <nandininayar@Nandinis-MacBook-Pro.local>
2020-12-14 22:22:23 +0530

    print loss every epoch

--- ensemble_alg.py
+++ ensemble_alg.py
@@ -80,16 +80,16 @@ def train_and_eval_model(network_name, dataset, trainloader, valloader, batch_si
             # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
-            print(epoch,loss)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+            # if i % 2000 == 1999:    # print every 2000 mini-batches
+            #     print('[%d, %5d] loss: %.3f' %
+            #           (epoch + 1, i + 1, running_loss / 2000))
+            #     running_loss = 0.0
+        print(epoch,running_loss)
 
     print('Finished Training '+network_name)
     val_loss = test(valloader, net)
