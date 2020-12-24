5050c8999344e0ae1ec140959072a8b6ad58e34d
Nandini Nayar <nandininayar@Nandinis-MacBook-Pro.local>
2020-12-24 18:20:49 +0530

    rerun all experiment set 3 with updated baseline7

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 ensemble_alg.py --dataset "cifar10" --filtered True --num_iters 5 --num_epochs 5
-python3 ensemble_alg.py --dataset "mnist" --filtered False --num_iters 5 --num_epochs 5
-python3 ensemble_alg.py --dataset "cifar10" --filtered False --num_iters 5 --num_epochs 5
+python3 ensemble_alg.py --dataset "cifar10" --filtered True --num_iters 5 --num_epochs 5 --num_classes 2
+python3 ensemble_alg.py --dataset "mnist" --filtered False --num_iters 5 --num_epochs 4 --num_classes 10
+python3 ensemble_alg.py --dataset "cifar10" --filtered False --num_iters 5 --num_epochs 6 --num_classes 10
